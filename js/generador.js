c249b03ab6c4429a2266111006d7e00d184a8830
bruno-cabrera98 <bruno.cabrera98@gmail.com>
2018-11-21 19:43:51 -0300

    json nuevo

--- js/generador.js
+++ js/generador.js
@@ -5,17 +5,21 @@ document.addEventListener("DOMContentLoaded", function (event) {
     request.responseType = 'json';
     request.send();
     request.onload = function () {
-        let titulos = document.getElementsByClassName("titulo");
+        let name = document.getElementsByClassName("titulo");
         let l_desc = document.getElementsByClassName("l_desc");
         let main_picture = document.getElementsByClassName("main_picture");
-        for(let i = 0; i < titulos.length; i++){
-            titulos[i].innerHTML = request.response.juegos[0].name;
+        let other_pictures = document.getElementsByClassName("imagen");
+        for(let i = 0; i < name.length; i++){
+            name[i].innerHTML = request.response.juegos[0].name;
         }
         for(let i = 0; i < l_desc.length; i++){
             l_desc[i].innerHTML = request.response.juegos[0].l_desc;
         }
         for(let i = 0; i < main_picture.length; i++){
             main_picture[i].innerHTML = "<img src='" + request.response.juegos[0].main_picture + "' alt='Foto portada'>";
+        }
+        for(let i = 0; i < other_pictures.length; i++){
+            other_pictures[i].innerHTML = "<img src='" + request.response.juegos[0].other_pictures + "' alt='Foto del juego'>";
         }
     }
 });
