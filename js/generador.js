83ad7021878d31bbb65576caccd3a5be3fab50b0
bruno-cabrera98 <bruno.cabrera98@gmail.com>
2018-11-21 19:29:01 -0300

    json nuevo

--- js/generador.js
+++ js/generador.js
@@ -7,12 +7,15 @@ document.addEventListener("DOMContentLoaded", function (event) {
     request.onload = function () {
         let titulos = document.getElementsByClassName("titulo");
         let l_desc = document.getElementsByClassName("l_desc");
-        console.log(titulos.length);
+        let main_picture = document.getElementsByClassName("main_picture");
         for(let i = 0; i < titulos.length; i++){
             titulos[i].innerHTML = request.response.juegos[0].name;
         }
         for(let i = 0; i < l_desc.length; i++){
             l_desc[i].innerHTML = request.response.juegos[0].l_desc;
+        }
+        for(let i = 0; i < main_picture.length; i++){
+            main_picture[i].innerHTML = "<img src='" + request.response.juegos[0].main_picture + "' alt='Foto portada'>";
         }
     }
 });
