6bf9ecaecfef9a3a603308f020024756573051b9
bruno-cabrera98 <bruno.cabrera98@gmail.com>
2018-11-21 22:09:11 -0300

    json nuevo

--- js/generador.js
+++ js/generador.js
@@ -4,36 +4,49 @@ document.addEventListener("DOMContentLoaded", function (event) {
     request.open("GET", url);
     request.responseType = 'json';
     request.send();
-    request.onload = function () {
+    request.onload = function () { //Carga la página usando el json
         let name = document.getElementsByClassName("titulo");
         let l_desc = document.getElementsByClassName("l_desc");
         let main_picture = document.getElementsByClassName("main_picture");
         let other_pictures = document.getElementsByClassName("imagen");
         let score_stars = document.getElementsByClassName("estrella");
-        let score = document.getElementById("puntos")
-        for(let i = 0; i < name.length; i++){
+        let score = document.getElementById("puntos");
+        let requirements_type = [document.getElementById("caja-specs-minimos"), document.getElementById("caja-specs-recomendados")];
+
+        for (let i = 0; i < name.length; i++) {
             name[i].innerHTML = request.response.juegos[0].name;
-        }
-        for(let i = 0; i < l_desc.length; i++){
+        };
+        for (let i = 0; i < l_desc.length; i++) {
             l_desc[i].innerHTML = request.response.juegos[0].l_desc;
-        }
-        for(let i = 0; i < main_picture.length; i++){
+        };
+        for (let i = 0; i < main_picture.length; i++) {
             main_picture[i].innerHTML = "<img src='" + request.response.juegos[0].main_picture + "' alt='Foto portada'>";
-        }
-        for(let i = 0; i < other_pictures.length; i++){
+        };
+        for (let i = 0; i < other_pictures.length; i++) {
             other_pictures[i].innerHTML = "<img src='" + request.response.juegos[0].other_pictures[i] + "' alt='Foto del juego'>";
-        }
-        for(let i = 0; i < score_stars.length; i++){
-            if (i < request.response.juegos[0].score){
+        };
+        for (let i = 0; i < score_stars.length; i++) {
+            if (i < request.response.juegos[0].score) {
                 score_stars[i].innerHTML = "<img src='img/estrella-amarilla.svg' alt='estrella-dorada'></img>"
             }
-            else{
+            else {
                 score_stars[i].innerHTML = "<img src='img/estrella-gris.svg' alt='estrella-gris'></img>"
             }
-        }
+        };
         score.innerHTML = request.response.juegos[0].score;
-        
+        let requirements = [request.response.juegos[0].requirements.minimum, request.response.juegos[0].requirements.recomended];
+        for (let i = 0; i < 2; i++) {
+            requirements_type[i].getElementsByClassName("os")[0].innerHTML = requirements[i].os;
+            requirements_type[i].getElementsByClassName("cpu")[0].innerHTML = requirements[i].cpu;
+            requirements_type[i].getElementsByClassName("ram")[0].innerHTML = requirements[i].ram;
+            requirements_type[i].getElementsByClassName("internet")[0].innerHTML = requirements[i].internet;
+            requirements_type[i].getElementsByClassName("gpu")[0].innerHTML = requirements[i].gpu;
+            requirements_type[i].getElementsByClassName("space")[0].innerHTML = requirements[i].space;
+        }
 
-        
+
+
+
+
     }
 });
