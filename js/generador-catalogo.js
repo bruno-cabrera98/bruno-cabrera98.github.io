e0eb52ac021c46a72a40aafde7dd8edb38c3a4ef
bruno-cabrera98 <bruno.cabrera98@gmail.com>
2018-11-23 15:55:42 -0300

    json nuevo

--- js/generador-catalogo.js
+++ js/generador-catalogo.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
     request.onload = function () {
         for (let i = 0; i < 8; i++){  //este for es para rellenar la página
             for (let k = 0; k < 4; k++){
-                contenedor.innerHTML += "<li class='bloque'><a href='" + request.response.juegos[k].link + "'><h1>" + request.response.juegos[k].name + "</h1><img src='" + request.response.juegos[k].main_picture + "'alt='foto de" + request.response.juegos[k].name + "'><div class='info'><p class='categoria'>" + request.response.juegos[k].categories[0] + "-" + request.response.juegos[k].categories[1] + "-" +  request.response.juegos[k].categories[2] + "</p><p class='precio'>$" + request.response.juegos[k].price + "</p></div></a></li>"; 
+                contenedor.innerHTML += "<li class='bloque'><a href='" + request.response.juegos[k].link + "'><h1>" + request.response.juegos[k].name + "</h1><img src='" + request.response.juegos[k].main_picture + "'alt='foto de " + request.response.juegos[k].name + "'><div class='info'><p class='categoria'>" + request.response.juegos[k].categories[0] + "-" + request.response.juegos[k].categories[1] + "-" +  request.response.juegos[k].categories[2] + "</p><p class='precio'>$" + request.response.juegos[k].price + "</p></div></a></li>"; 
             }
         }
     }
